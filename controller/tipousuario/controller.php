0710bab9a5141c58e1b1624f3e5629af5865b96e
Lucke0302 <lucke0302@gmail.com>
2022-05-30 07:25:58 -0300

    tcc

--- controller/tipousuario/controller.php
+++ controller/tipousuario/controller.php
@@ -4,7 +4,9 @@ $id = $_SESSION['id'];
 include('../../model/atributosusuario.php');
 $tipo = new Selecionar($id);
 if($_SESSION['Logado'] == true){
-    echo "<li><a href='../perfil/'><ion-icon name='person-outline'></ion-icon>Perfil</a></li>";
+    echo "<li><a href='../perfil/'><ion-icon name='person-outline'></ion-icon>Perfil</a></li>
+          <li><a href='../../controller/encerrarsessao/controller.php'><ion-icon name='log-out-outline'></ion-icon>Sair</a></li>";
+    
     if($tipo -> verificarAdm() == true){
         echo "<li><a href='../administrador/'><ion-icon name='code-working-outline'></ion-icon>Administrador</a></li>";
     } 
